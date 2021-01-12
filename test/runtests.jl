1351c4e89ae1a46139b783a2407edeb604a11602
Tim Schäfer <ts+code@rcmd.org>
2021-01-12 23:28:42 +0100

    move functions to main file

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,12 @@ using Test
 @testset "NeuroFormats.jl" begin
     
     CURV_LH_THICKNESS_FILE = joinpath(dirname(@__FILE__), "data/subjects_dir/subject1/surf/lh.thickness")
-    curv = readcurv(CURV_LH_THICKNESS_FILE)
+    curv = NeuroFormats.readcurv(CURV_LH_THICKNESS_FILE)
 
     # Header
     @test length(curv.header) == 4
   
     # Content
-    @test flength(curv.data) == 149000
+    @test length(curv.data) == 149000
     @assert maximum(curv.data) == 5.5
 end
