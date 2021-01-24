b28149a1a0a7703eb4106695d4690a1ee997ab59
Tim Schäfer <ts+code@rcmd.org>
2021-01-24 22:55:02 +0100

    Fix indentation only

--- test/test_fs_surface.jl
+++ test/test_fs_surface.jl
@@ -1,12 +1,12 @@
 @testset "fs_surface.jl: read brain mesh" begin
-    
-BRAIN_MESH_FILE = joinpath(get_testdata_dir(), "subjects_dir/subject1/surf/lh.tinysurface")
-surface = read_fs_surface(BRAIN_MESH_FILE)
+        
+    BRAIN_MESH_FILE = joinpath(get_testdata_dir(), "subjects_dir/subject1/surf/lh.tinysurface")
+    surface = read_fs_surface(BRAIN_MESH_FILE)
 
-# Header
-#@test surface.header.num_vertices == 5
+    # Header
+    #@test surface.header.num_vertices == 5
 
-# Content
-@test Base.ndims(surface.mesh.vertices) == 1
-@test Base.ndims(surface.mesh.faces) == 1
+    # Content
+    @test Base.ndims(surface.mesh.vertices) == 1
+    @test Base.ndims(surface.mesh.faces) == 1
 end
--- test/utils_for_testing.jl
+++ test/utils_for_testing.jl
@@ -4,12 +4,12 @@
 This is required because during tests, the base dir seems to be <package/test>, while it is <package> in the standard REPL.
 """
 function get_testdata_dir()
-if isdir("data/subjects_dir")
-    return joinpath(dirname(@__FILE__), "data/")
-elseif isdir("test/data/subjects_dir")
-    return joinpath(dirname(@__FILE__), "test/data/")
-else
-    error("Could not determine test data directory from current working directory.")
-end
-nothing
+    if isdir("data/subjects_dir")
+        return joinpath(dirname(@__FILE__), "data/")
+    elseif isdir("test/data/subjects_dir")
+        return joinpath(dirname(@__FILE__), "test/data/")
+    else
+        error("Could not determine test data directory from current working directory.")
+    end
+    nothing
 end
