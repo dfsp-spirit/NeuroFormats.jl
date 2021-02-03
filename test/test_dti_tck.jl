0f72519b8bd521d7e5d13fb5cb80d98750eaae9c
Tim Schäfer <ts+code@rcmd.org>
2021-02-03 23:00:29 +0100

    extend TCK header test

--- test/test_dti_tck.jl
+++ test/test_dti_tck.jl
@@ -5,6 +5,12 @@
     TCK_FILE = joinpath(get_testdata_dir(), "DTI/simple_big_endian.tck")
     tck = read_tck(TCK_FILE)
 
+    # test header
     @test typeof(tck.header["file"]) == String
+    @test tck.header["file"] == ". 67"
+    @test tck.header["datatype"] == "Float32BE"
+    @test tck.header["count"] == "0000000003"
+
+    # test data
 end
 
