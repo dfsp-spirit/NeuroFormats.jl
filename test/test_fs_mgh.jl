c4d87e0373e506099dc20cf2754cbe8752487d70
Tim Schäfer <ts+code@rcmd.org>
2021-02-10 23:14:41 +0100

    test MGH data

--- test/test_fs_mgh.jl
+++ test/test_fs_mgh.jl
@@ -6,6 +6,7 @@
     MGH_FILE = joinpath(get_testdata_dir(), "subjects_dir/subject1/mri/brain.mgz")
     mgh = read_mgh(MGH_FILE)
 
+    # Test header.
     @test mgh.header.mgh_version == 1
     @test mgh.header.ndim1 == 256
     @test mgh.header.ndim2 == 256
@@ -15,4 +16,11 @@
     @test mgh.header.dof == 0
     @test mgh.header.is_ras_good == 1
 
+    # TODO: test RAS data
+
+    # Test data.
+    @test Base.ndims(mgh.data) == 4
+    @test mgh.data[100, 100, 100, 1] == 77   # try on command line: mri_info --voxel 99 99 99 test/data/subjects_dir/subject1/mri/brain.mgz
+    @test mgh.data[110, 110, 110, 1] == 71
+    @test mgh.data[1, 1, 1, 1] == 0
 end
